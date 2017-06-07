6871147e1b200295aaa85aca5e087131bb1ca688
SteveQQ <sawicki.adam172@gmail.com>
2017-06-07 22:14:01 +0200

    Initial commit

---
+++ src/main/java/com/steveq/HelloHeroku/HelloHerokuApplication.java
@@ -0,0 +1,12 @@
+package com.steveq.HelloHeroku;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloHerokuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloHerokuApplication.class, args);
+	}
+}
